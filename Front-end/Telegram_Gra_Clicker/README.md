ea1aed485b846fc1dd898322629a1218d50d12f6
DenysKotsiuba <denys.kotsiuba25@gmail.com>
2025-01-05 12:59:21 +0100

    update

---
+++ Front-end/Telegram_Gra_Clicker/README.md
@@ -0,0 +1,6 @@
+Telegram Gra Clicker!
+Link dla przykładowego obejrzenia działania: https://clicker-projekt.web.app/
+Przed uruchomieniem należy utworzyć w folderze "/bot" plik tokien.js z następną zawartością:
+"const token = 'Your_Telegram_Tokien'
+const webAppUrl = 'link_na_hosting'
+export { token, webAppUrl };"
---
+++ Front-end/Telegram_Gra_Clicker/src/stores/app.js
@@ -0,0 +1,32 @@
+import { getOrCreateUser, registerRef, fetchTasks, completeTask } from "@/api/app"
+import { defineStore } from "pinia"
+import { useScoreStore } from "./score"
+import { useTelegram } from "@/services/telegram"
+
+const { user } = useTelegram()
+
+export const useAppStore = defineStore('app', {
+    state: () => ({
+       user: {},
+       tasks: {}, 
+    }),
+    actions: {
+       async init(ref) {
+        this.user = await getOrCreateUser()
+        
+        const score = useScoreStore()
+
+        score.setScore(this.user.score)
+        
+        if (ref && +ref !== +this.user.telegram) {
+         await registerRef(this.user?.first_name ?? "Clicker", ref)
+        }
+       }, 
+       async completeTask(task) {
+         await completeTask(this.user, task)
+       },
+       async fetchTasks() {
+         this.tasks = await fetchTasks()
+       },
+    },
+})
