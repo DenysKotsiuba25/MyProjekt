ce368082a080b20c8406700c494a3de83731d099
DenysKotsiuba <denys.kotsiuba25@gmail.com>
2025-03-20 14:38:27 +0100

    test

--- Front-end/Calculator/app.js
+++ Front-end/Calculator/app.js
@@ -4,6 +4,7 @@ const buttons = document.querySelectorAll(".buttons button");
 let currentInput = "";
 let operator = "";
 let firstNumber = "";
+let isResultDisplayed = false;  
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
@@ -11,8 +12,14 @@ buttons.forEach(button => {
 
     if (!isNaN(value)) {
       
-      currentInput += value;
-      result.textContent = currentInput;
+      if (isResultDisplayed) {
+        currentInput = value;
+        result.textContent = currentInput;
+        isResultDisplayed = false;  
+      } else {
+        currentInput += value;
+        result.textContent = currentInput;
+      }
     } else if (["+", "-", "*", "/"].includes(value)) {
       
       if (currentInput !== "") {
@@ -20,19 +27,22 @@ buttons.forEach(button => {
         operator = value;
         currentInput = "";
       }
+      isResultDisplayed = false;  
     } else if (value === "=") {
-      
+       
       if (firstNumber !== "" && currentInput !== "") {
         result.textContent = eval(`${firstNumber} ${operator} ${currentInput}`);
         firstNumber = "";
         currentInput = result.textContent;
+        isResultDisplayed = true;  
       }
     } else if (value === "C") {
-     
+      
       firstNumber = "";
       operator = "";
       currentInput = "";
       result.textContent = "0";
+      isResultDisplayed = false;  
     }
   });
 });
